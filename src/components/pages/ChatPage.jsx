b8dca8561f9d5d697e2cba44094ced1570c55659
Ivan Orpiano <ivanritchelorpiano@gmail.com>
2026-06-17 22:37:13 +0800

    Phase 1 Chat Page Development

---
+++ src/components/pages/ChatPage.jsx
@@ -0,0 +1,5 @@
+import React, {useCallback, useState }  from 'react';
+import {useChat} from '../hooks/useChats';
+import ChatWindow from '../components/chat/ChatWindow';
+import ChatInput from '../components/input/ChatInput';
+
