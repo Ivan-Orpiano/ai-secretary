61e2054c0d5f05668f5408c19e8f1841f5e32272
Ivan Orpiano <ivanritchelorpiano@gmail.com>
2026-06-17 22:59:57 +0800

    Phase 1 Chat Page Development

--- src/components/pages/ChatPage.jsx
+++ src/components/pages/ChatPage.jsx
@@ -3,3 +3,13 @@ import {useChat} from '../hooks/useChats';
 import ChatWindow from '../components/chat/ChatWindow';
 import ChatInput from '../components/input/ChatInput';
 
+export default function ChatPage() {
+    const {messages, isLoading, sendMessage } = useChat ();
+    const [prefill, setPrefill] = useState ('');
+
+    const handleSuggestionSelect = useCallback((text) => {
+        
+    })
+
+
+}
